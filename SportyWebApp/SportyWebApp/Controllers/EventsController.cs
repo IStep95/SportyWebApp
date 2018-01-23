29b6ca0d309d70899356c9047a8dd0314cf78c88
Ivan Stepanic <stepanic.ivan@gmail.com>
2018-01-23 22:05:49 +0100

    Init find page

---
+++ SportyWebApp/SportyWebApp/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using SportyWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportyWebApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Search
+        public ActionResult Search(string sport, string date, string city, string freePlayers)
+        {
+            //List<EventViewModel> todayEvents = await _api.HttpGetTodayEventsByCityId(_userViewModel.CityId);
+            //foreach (var entry in todayEvents)
+            //{
+            //    var sportName = entry.SportName;
+            //    if (!String.IsNullOrEmpty(sportName))
+            //    {
+            //        sportName = sportName.First().ToString().ToUpper() + sportName.Substring(1);
+            //        entry.SportName = sportName;
+            //    }
+            //}
+
+            ViewBag.CurrentPage = "SearchEventPage";
+            return View();
+        }
+
+    }
+}
--- SportyWebApp/SportyWebApp/Models/LoginViewModel.cs
+++ SportyWebApp/SportyWebApp/Models/LoginViewModel.cs
@@ -9,7 +9,6 @@ namespace SportyWebApp.Models
     {
         public string UserName { get; set; }
         public string Password { get; set; }
-
         public bool UserNotExist { get; set; }
     }
 }
